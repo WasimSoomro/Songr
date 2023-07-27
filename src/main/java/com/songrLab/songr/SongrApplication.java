df631b4330902721cd5a6339641015f35b3fbaba
Wasim Soomro <103798599+WasimSoomro@users.noreply.github.com>
2023-07-27 09:36:04 -0400

    Post

--- src/main/java/com/songrLab/songr/SongrApplication.java
+++ src/main/java/com/songrLab/songr/SongrApplication.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.songrLab.songr.Album;
 
 @SpringBootApplication
 public class SongrApplication {
@@ -17,6 +21,20 @@ public class SongrApplication {
 
 	@Controller
 	public class HelloController {
+
+		@PostMapping("/album")
+		public String addAlbum(@RequestParam String title, @RequestParam String artist, @RequestParam int songCount, @RequestParam int length, @RequestParam String imageUrl, Model model) {
+			// Create a new album and add it to the database
+
+			// for now, you can simply add it to your model
+			Album newAlbum = new Album(title, artist, songCount, length, imageUrl);
+			// you may need to adjust this line based on how you are storing albums
+			model.addAttribute("album", newAlbum);
+
+			// you could redirect to the album page to show the newly created album
+			return "redirect:/album";
+		}
+
 		@GetMapping("/")
 		public String getSplashPage() {
 			return "splash.html";
